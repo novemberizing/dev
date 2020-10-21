08745d99ce9485493bafa7a83549cd965634a76a
novemberizing <novemberizing@gmail.com>
2020-10-21 19:41:16 +0900

    daily commit

--- pubsub/README.md
+++ pubsub/README.md
@@ -17,3 +17,37 @@ https://blog.naver.com/PostView.nhn?blogId=wnrjsxo&logNo=221338322734
 https://javascript.info/server-sent-events
 
 'EventSource'
+
+https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
+
+
+start a redis instance
+$ docker run --name some-redis -d redis
+start with persistent storage
+$ docker run --name some-redis -d redis redis-server --appendonly yes
+If persistence is enabled, data is stored in the VOLUME /data, which can be used with --volumes-from some-volume-container or -v /docker/host/dir:/data (see docs.docker volumes).
+
+For more about Redis Persistence, see http://redis.io/topics/persistence.
+
+
+
+```
+sudo docker run -it --rm --name my-second-redis --link my-first-redis:redis -d redis
+```
+
+docker run --name some-redis -d redis
+
+
+```
+docker build -t novemberizing-pubsub .
+```
+
+docker run -it --rm --name pubsub --link some-redis:redis novemberizing-pubsub
+
+
+docker run --name textory-redis -p 6379:6379 -d redis
+
+
+```
+docker run --name [name] -p 6379:6379 -d redis
+```
--- pubsub/server/index.js
+++ pubsub/server/index.js
@@ -1,10 +1,60 @@
 const express = require('express');
-
+const redis = require('redis');
 const app = express();
 
-class Pubsub {
-    construct() {
+const retry = {
+    retry_strategy: function(options) {
+        if (options.total_retry_time > 1000 * 60 * 60) {
+            // reconnect after
+            return 10000;
+        }
+        if (options.error && options.error.code === "ECONNREFUSED") {
+            // reconnect after
+            return 5000;
+        }
+        // reconnect after
+        if(options.attempt > 50) {
+            options.attempt = 0;
+        }
+        return Math.min(options.attempt * 100, 3000);
+    }
+};
 
+class Pubsub {
+    constructor() {
+        this.client = redis.createClient(retry);
+        this.subscriber = redis.createClient(retry);
+
+
+
+        this.client.on('error', (e) => { console.log(e); });
+        this.client.on('ready', (o) => {});
+        this.client.on('connect', (o) => {
+            console.log('send all data');
+        });
+        this.client.on('reconnecting', (o) => { console.log('reconnect'); });
+        this.client.on('end', (o) => {});
+        this.client.on('warning', (o) => {});
+
+        this.subscriber.on('error', (e) => { console.log(e); });
+        this.subscriber.on('ready', (o) => {});
+        this.subscriber.on('connect', (o) => {});
+        this.subscriber.on('reconnecting', (o) => { console.log('reconnect'); });
+        this.subscriber.on('end', (o) => {});
+        this.subscriber.on('warning', (o) => {});
+        this.subscriber.on('subscribe', (channel, count) => {});
+        this.subscriber.on('message', async (channel, message) => {
+            console.log(message);
+            const session = clients[message];
+            if(session) {
+                const data = await this.get(message);
+                session.res.write(`data: ${JSON.stringify({message:data})}\n\n`);
+                console.log('session write');
+            } else {
+                console.log('no session');
+            }
+        });
+        this.subscriber.subscribe("message");
     }
 
     async auth(auth) {
@@ -20,15 +70,47 @@ class Pubsub {
     }
 
     async get(key) {
-
+        return new Promise((resolve, reject) => {
+            this.client.hgetall(key, (e, v) => {
+                if(e) {
+                    reject(e);
+                    return;
+                }
+                console.log(v);
+                resolve(v);
+            });
+        });
     }
 
-    async del(key, keys) {
-
+    async del(key, data) {
+        return new Promise((resolve, reject) => {
+            console.log(data);
+            this.client.hdel(key, data.key, (e, v) => {
+                if(e) {
+                    reject(e);
+                    return;
+                }
+                resolve(v);
+            });
+        });
     }
 
     async put(key, data) {
-
+        return new Promise((resolve, reject) => {
+            this.client.hset(key, data.key, data.value, (e, v) => {
+                if(e) {
+                    reject(e);
+                    return;
+                }
+                this.client.publish('message', key, (e, v) => {
+                    if(e) {
+                        reject(e);
+                        return;
+                    }
+                    resolve(v);
+                });
+            });
+        });
     }
 };
 
@@ -39,13 +121,30 @@ const template = `<!DOCTYPE html>
     <head>
     </head>
     <body>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <script type="text/javascript">
-            const source = new EventSource("/events/?auth=" + 93, {
+            const auth = '93';
+            const source = new EventSource("/events/?auth=" + auth, {
                 withCredentials: false
             });
             console.log(source);
             source.onmessage = function(e) {
                 document.body.innerHTML += e.data + "<br>";
+                const data = JSON.parse(e.data);
+                console.log(data.message);
+                if(data.message) {
+                    Object.keys(data.message).forEach(async (k) => {
+                        console.log(k);
+                        try {
+                            document.body.innerHTML += JSON.stringify({key: data.message[k]}) + "<br>";
+                            const result = await axios.get('/del/?auth='+auth+'&data='+JSON.stringify({key:k}));
+                            document.body.innerHTML += JSON.stringify(result.data) + "<br>";
+                        } catch(e) {
+                            console.log(e.toString());
+                        }
+
+                    });
+                }
             }
         </script>
     </body>
@@ -53,17 +152,20 @@ const template = `<!DOCTYPE html>
 
 let clients = {};
 
+// REMOVE THIS : RELEASE
 app.get('/', (req, res) => {
     res.send(template);
 });
 
 app.get('/del/', async (req, res) => {
     try {
+        console.log('del');
         const key = await service.auth(req.query.auth);
-        const data = JSON.stringify(req.query.data);
+        const data = JSON.parse(req.query.data);
+        console.log(data);
         if(key) {
             const result = await service.del(key, data);
-            res.send(result);
+            res.send(JSON.stringify(result));
         } else {
             // logging invalid access must close
             console.log('invalid access => without auth key');
@@ -77,11 +179,12 @@ app.get('/del/', async (req, res) => {
 
 app.get('/put/', async (req, res) => {
     try {
+        console.log('put');
         const key = await service.auth(req.query.auth);
-        const data = JSON.stringify(req.query.data);
+        const data = JSON.parse(req.query.data);
         if(key) {
             const result = await service.put(key, data);
-            res.send(result);
+            res.send(JSON.stringify(result));
         } else {
             // logging invalid access must close
             console.log('invalid access => without auth key');
@@ -125,10 +228,14 @@ app.get('/events/', async (req, res) => {
                 'Connection': 'keep-alive'
             });
             res.write('\n');
-            res.write(`data: ${JSON.stringify({message:'greeting'})}\n\n`);
+            res.write(`data: ${JSON.stringify({greeting:'greeting'})}\n\n`);
             // 4. SEND REAL DATA
-            const data = await service.get(session.auth);
-            res.write(`data: ${JSON.stringify(data)}`);
+            const data = await service.get(session.key);
+            if(data) {
+                res.write(`data: ${JSON.stringify({message: data})}\n\n`);
+            } else {
+                console.log('no data');
+            }
         } else {
             // logging invalid access must close
             console.log('invalid access => without auth key');
