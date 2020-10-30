fffdbed02fa897a3d706b5477d5b82fb69f7faa4
novemberizing <novemberizing@gmail.com>
2020-10-30 20:47:46 +0900

    daily commit!

--- bootstrap/src/component/ui/topbar/navigation/alarm/index.js
+++ bootstrap/src/component/ui/topbar/navigation/alarm/index.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Reacts from '../../../../../util/reacts';
 import TopbarNavDropdown from '../dropdown';
 
 export default class TopbarNavAlarm extends TopbarNavDropdown {
@@ -14,45 +15,47 @@ export default class TopbarNavAlarm extends TopbarNavDropdown {
                     <FontAwesomeIcon icon={["fas", "bell"]} fixedWidth size="1x" />
                     <span className="badge badge-danger badge-counter">3</span>
                 </a>
-                <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in show" aria-labelledby="alertsDropdown">
-                    <h6 class="dropdown-header">
-                        Alerts Center
-                    </h6>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
-                        <div class="mr-3">
-                            <div class="icon-circle bg-primary">
-                                <i class="fas fa-file-alt text-white"></i>
+                {Reacts.display(this.state.visible,
+                    <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in show" aria-labelledby="alertsDropdown">
+                        <h6 className="dropdown-header">
+                            Alerts Center
+                        </h6>
+                        <a className="dropdown-item d-flex align-items-center" href="#">
+                            <div className="mr-3">
+                                <div className="icon-circle bg-primary">
+                                    <i className="fas fa-file-alt text-white"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <div class="small text-gray-500">December 12, 2019</div>
-                            <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                        </div>
-                    </a>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
-                        <div class="mr-3">
-                            <div class="icon-circle bg-success">
-                                <i class="fas fa-donate text-white"></i>
+                            <div>
+                                <div className="small text-gray-500">December 12, 2019</div>
+                                <span className="font-weight-bold">A new monthly report is ready to download!</span>
                             </div>
-                        </div>
-                        <div>
-                            <div class="small text-gray-500">December 7, 2019</div>
-                            $290.29 has been deposited into your account!
-                        </div>
-                    </a>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
-                        <div class="mr-3">
-                            <div class="icon-circle bg-warning">
-                                <i class="fas fa-exclamation-triangle text-white"></i>
+                        </a>
+                        <a className="dropdown-item d-flex align-items-center" href="#">
+                            <div className="mr-3">
+                                <div className="icon-circle bg-success">
+                                    <i className="fas fa-donate text-white"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <div class="small text-gray-500">December 2, 2019</div>
-                            Spending Alert: We've noticed unusually high spending for your account.
-                        </div>
-                    </a>
-                    <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                </div>
+                            <div>
+                                <div className="small text-gray-500">December 7, 2019</div>
+                                $290.29 has been deposited into your account!
+                            </div>
+                        </a>
+                        <a className="dropdown-item d-flex align-items-center" href="#">
+                            <div className="mr-3">
+                                <div className="icon-circle bg-warning">
+                                    <i className="fas fa-exclamation-triangle text-white"></i>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="small text-gray-500">December 2, 2019</div>
+                                Spending Alert: We've noticed unusually high spending for your account.
+                            </div>
+                        </a>
+                        <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                    </div>
+                )}
             </li>
         );
     }
