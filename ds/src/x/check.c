f392d64294cee52f148652811015f9bb1166c29f
novemberizing <novemberizing@gmail.com>
2020-12-28 11:54:42 +0900

    update log

--- ds/src/x/check.c
+++ ds/src/x/check.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+#include <x/log.h>
+
 int main(int argc, char ** argv)
 {
-    
+    functionLog("%s %s", "hello", "world");
+    criticalLog("%s %s", "hello", "world");
+    errorLog("%s %s", "hello", "world");
+    warningLog("%s %s", "hello", "world");
+    cautionLog("%s %s", "hello", "world");
+    noticeLog("%s %s", "hello", "world");
+    informationLog("%s %s", "hello", "world");
+    debugLog("%s %s", "hello", "world");
+    assertion(false, "hello world");
+
     return 0;
 }
--- ds/src/x/log.h
+++ ds/src/x/log.h
@@ -1,13 +1,24 @@
 #ifndef   __NOVEMBERIZING_X__LOG__H__
 #define   __NOVEMBERIZING_X__LOG__H__
 
-#define xfunctionflow
-#define xcriticallog
-#define xerrorlog
-#define xwarninglog
-#define xcautionlog
-#define xnoticelog
-#define xinformationlog
-#define xdebuglog
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#define functionLog(format, ...)    fprintf(stdout, "[flow] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define criticalLog(format, ...)    fprintf(stdout, "[critical] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define errorLog(format, ...)       fprintf(stdout, "[error] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define warningLog(format, ...)     fprintf(stdout, "[warning] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define cautionLog(format, ...)     fprintf(stdout, "[caution] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define noticeLog(format, ...)      fprintf(stdout, "[notice] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define informationLog(format, ...) fprintf(stdout, "[information] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define debugLog(format, ...)       fprintf(stdout, "[debug] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__)
+#define assertion(x, format, ...)   do {                                                                                    \
+    if(x) {                                                                                                                 \
+        fprintf(stdout, "[assert] %s/%ld " __FILE__ ":%d " format "\n", __func__, pthread_self(), __LINE__, ##__VA_ARGS__); \
+        abort();                                                                                                            \
+    }                                                                                                                       \
+} while(0)
 
 #endif // __NOVEMBERIZING_X__LOG__H__
