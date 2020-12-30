46991dc5ce85f29767a236f7e35cb531cb3e9889
novemberizing <novemberizing@gmail.com>
2020-12-30 17:04:31 +0900

    check

--- core/src/x/types.c
+++ core/src/x/types.c
@@ -1,2 +1,17 @@
 #include "types.h"
+#include "log.h"
 
+xobj * xobjrem(xobj * o)
+{
+    xcheck(o == xnil, xnil, "o == xnil");
+
+    xassertion(xobjhas(o, xallocated) == false, "xobjhas(o, xallocated) == false");
+
+    if(o->rem)
+    {
+        o->rem(o);
+    }
+    free(o);
+    
+    return xnil;
+}
--- core/src/x/types.h
+++ core/src/x/types.h
@@ -6,6 +6,7 @@
 #define xnil                (void *)(0)
 #define xsuccess            0
 #define xfail               -1
+#define xallocated          0x80000000U
 
 typedef __INT8_TYPE__       xint8;
 typedef __INT16_TYPE__      xint16;
@@ -16,6 +17,30 @@ typedef __UINT16_TYPE__     xuint16;
 typedef __UINT32_TYPE__     xuint32;
 typedef __UINT64_TYPE__     xuint64;
 
+union xval
+{
+    xuint32 u32;
+    xuint64 u64;
+    void *  ptr;
+};
+
+typedef union xval xval;
+
+struct xobj;
+
+typedef struct xobj xobj;
+typedef xobj * (*destructor)(xobj *);
+typedef xobj * (*constructor)(void);
+
+struct xobj
+{
+    xuint32 flags;
+    destructor rem;
+};
+
+#define xobjhas(o, v) (o->flags & v)
+
+extern xobj * xobjrem(xobj * o);
 
 
 #endif // __NOVEMBERIZING_X__TYPES__H__
