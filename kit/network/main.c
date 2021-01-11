6b157e098dbe80957ec7b0f6bdd256f0b57e9947
novemberizing <novemberizing@gmail.com>
2021-01-11 09:56:55 +0900

    update daily!

--- kit/network/main.c
+++ kit/network/main.c
@@ -1,23 +1,84 @@
 #include <stdio.h>
 
-struct xeventengine;
+#define xinvalid    -1
+#define xsuccess    0
+#define xfail       -1
+#define xtrue       1
+#define xfalse      0
+#define xnil        (void *)(0)
 
-struct xeventengine
+typedef __INT8_TYPE__       xint8;
+typedef __INT16_TYPE__      xint16;
+typedef __INT32_TYPE__      xint32;
+typedef __INT64_TYPE__      xint64;
+typedef __UINT8_TYPE__      xuint8;
+typedef __UINT16_TYPE__     xuint16;
+typedef __UINT32_TYPE__     xuint32;
+typedef __UINT64_TYPE__     xuint64;
+
+#define xassertion(condition, format, ...) do {     \
+    if(condition) {                                 \
+        dprintf(xlogfd(), "[assertion] "            \
+        "%s:%d "                                    \
+        "%s:%lu => "                                \
+        format "\n",                                \
+        __FILE__, __LINE__,                         \
+        __func__, xthreadid(),                      \
+        ##__VA_ARGS__);                             \
+    }                                               \
+} while(0)
+
+struct xsocket;
+
+typedef struct xsocket xsocket;
+
+struct xsocket
 {
-
+    int fd;
 };
 
-extern xeventengine * xevent_engine_new();
+struct xgenerator;
+
+typedef struct xgenerator xgenerator;
+
+struct xgenerator
+{
+    xgenerator * next;
+};
+
+struct xengine;
+
+typedef struct xengine xengine;
+
+#define xengine_status_void         0x00000000U
+#define xengine_status_cancel       0x80000000U
+
+struct xengine
+{
+    xuint32 status;
+    xgenerator * generators;
+};
+
+#define xengineinit()   (xengine) { xengine_status_void }
+
+extern int xenginerun(xengine * o);
 
 int main(int argc, char ** argv)
 {
-    xeventengine * engine = xevent_engine_new();
+    xengine engine = xengineinit();
 
-    xevent_engine_run(engine);
-    return 0;
+    return xenginerun(&engine);
 }
 
-xeventengine * xevent_engine_new()
+int xenginerun(xengine * o)
 {
-    return 0;
+    xassertion(o == xnil, "invalid parameter");
+    while((o->status & xengine_status_cancel) == xfalse)
+    {
+        for(xgenerator * generator = o->generators; generator != xnil; generator = generator->next)
+        {
+            xgeneratorrun(generator, o);
+        }
+    }
+    return xsuccess;
 }
