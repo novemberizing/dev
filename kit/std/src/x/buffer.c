96f9ee5ed875f57f727fd8238f6f0d9e95f6f3a5
novemberizing <novemberizing@gmail.com>
2021-01-12 19:19:44 +0900

    set doxygen configure.

--- kit/std/src/x/buffer.c
+++ kit/std/src/x/buffer.c
@@ -1,14 +1,27 @@
+/**
+ * @file    x/buffer.c
+ * @brief   버퍼 파일
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "std.h"
 
 /**
+ * @def     xbufferfront(buffer)
+ * @brief   버퍼 객체의 내부 데이터 주소 값 (사용한 후의 버퍼의 주소)
+ * @details
+ * 
+ * @param   | buffer | in | xbuffer * | 버퍼 객체 |
+ */
+
+
+/**
  * @fn      xbuffer * xbuffernew(xuint64 capacity)
  * @brief   기본 버퍼 객체를 생성합니다.
  * @details
  * 
- * @param   | capacity | in | xuint64 | 버퍼의 크기 |
+ * @param   capacity | capacity | in | xuint64 | 버퍼의 크기 |
  * 
  * @return  | xbuffer * | 버퍼 객체 |
  */
@@ -31,7 +44,7 @@ xbuffer * xbuffernew(xuint64 capacity)
  * @brief   버퍼 객체를 메모리에서 해제 합니다.
  * @details
  * 
- * @param   | p | in | void * | 버퍼 객체의 메모리 주소 |
+ * @param   p   | in | void * | 버퍼 객체의 메모리 주소 |
  * @return  | void * | 해제된 버퍼 객체의 메모리 주소 |
  *          
  *              버퍼 객체가 동적할당에 의해서 생성되었으면 널을 리턴합니다.
--- kit/std/src/x/thread.c
+++ kit/std/src/x/thread.c
@@ -2,6 +2,13 @@
 
 #include "std.h"
 
+/**
+ * @fn      unsigned long xthreadid(void)
+ * @brief   스레드 아이디를 리턴합니다.
+ * @details
+ * 
+ * @return  | unsigned long | 스레드 아이디 |
+ */
 unsigned long xthreadid(void)
 {
     return pthread_self();
