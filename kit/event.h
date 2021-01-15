d6af1c3a9f3c47facd21b05fecf37673a6fa63df
novemberizing <novemberizing@gmail.com>
2021-01-15 18:38:47 +0900

    refactoring

--- kit/event.h
+++
@@ -1,7 +0,0 @@
-#ifndef   __NOVEMBERIZING_X__EVENT__H__
-#define   __NOVEMBERIZING_X__EVENT__H__
-
-
-#include <x/std.h>
-
-#endif // __NOVEMBERIZING_X__EVENT__H__
--- kit/list.c
+++
@@ -1,6 +0,0 @@
-#include <x/std.h> 
-
-int main(int argc, char ** argv)
-{
-    return 0;
-}
