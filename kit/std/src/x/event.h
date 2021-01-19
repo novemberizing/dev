76e8b710d5e833e072122fcaa9b1034937fe40f1
novemberizing <novemberizing@gmail.com>
2021-01-19 19:40:11 +0900

    implement event new

--- kit/std/src/x/event.h
+++ kit/std/src/x/event.h
@@ -11,10 +11,29 @@ typedef struct xevent xevent;
 typedef struct xeventgenerator xeventgenerator;
 typedef struct xeventengine xeventengine;
 
-#define xeventengine_status_void        0x00000000U
-#define xeventengine_status_on          0x00000001U
+#define xevent_mask_categories                  0xFF000000U
+#define xevent_mask_types                       0x0000FFFFU
 
-#define xeventengine_status_cancel      0x80000000U
+#define xevent_category_io                      0x01000000U
+#define xevent_category_signal                  0x02000000U
+#define xevent_category_time                    0x03000000U
+
+#define xevent_category_custom                  0xFF000000U
+
+#define xeventengine_mask_status                0x00FFFFFFU
+
+#define xeventengine_allocated                  0x80000000U
+
+#define xeventengine_status_void                0x00000000U
+#define xeventengine_status_on                  0x00000001U
+#define xeventengine_status_main_cusuming_off   0x00000002U
+#define xeventengine_status_cancel              0x00800000U
+
+#define xeventcategory(event)                   (event->type & xevent_mask_categories)
+#define xeventtype(event)                       (event->type & xevent_mask_types)
+
+#define xeventenginestatus(engine)              (engine->status & xeventengine_mask_status)
+#define xeventengineallocated(engine)           (engine->status & xeventengine_allocated)
 
 struct xevent
 {
@@ -33,7 +52,13 @@ struct xeventengine
     xlist generators;
     void (*internal)(xeventengine *);
     xlist queue;
+    xlist processors;
+    xevent * (*handler)(xevent *, xeventengine *);
 };
+
+#define xeventengineinit()  (xeventengine) { 0, xlistinit(), xnil, xlistinit(), xlistinit(), xnil }
+
+extern xeventengine * xeventenginenew(void);
 
 extern xint32 xeventenginerun(xeventengine * o);
 
--- kit/std/src/x/event/engine.c
+++ kit/std/src/x/event/engine.c
@@ -1,12 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "../event.h"
 
+static inline xevent * __xevent_internal_process_io(xevent * event, xeventengine * o)
+{
+    return xnil;
+}
+
+static inline xevent * __xevent_internal_process_signal(xevent * event, xeventengine * o)
+{
+    return xnil;
+}
+
+static inline xevent * __xevent_internal_process_time(xevent * event, xeventengine * o)
+{
+    return xnil;
+}
+
+static inline xevent * __xevent_internal_process_custom(xevent * event, xeventengine * o)
+{
+    xassertion(o->handler, "custom handler not exist");
+    return o->handler(event, o);
+}
+
 static inline xevent * __xevent_internal_process(xevent * event, xeventengine * o)
 {
-
+    switch(xeventcategory(event))
+    {
+        case xevent_category_io:     return __xevent_internal_process_io(event, o);
+        case xevent_category_signal: return __xevent_internal_process_signal(event, o);
+        case xevent_category_time:   return __xevent_internal_process_time(event, o);
+        case xevent_category_custom: return __xevent_internal_process_custom(event, o);
+    }
+    xassertion(xtrue, "unsupported event category");
     return xnil;
+}
+
+static inline void __xeventengine_main_consume(xeventengine * o)
+{
+    xsynclock(o->queue.sync);
+    xuint64 total = xlistsize(xaddressof(o->queue));
+    for(xuint64 i = 0; i < total && xlistsize(xaddressof(o->queue)) > 0; i++)
+    {
+        xlistnode * node = o->queue.head;
+        o->queue.head = node->next;
+        if(o->queue.head)
+        {
+            o->queue.head->prev = xnil;
+        }
+        else
+        {
+            o->queue.tail = xnil;
+        }
+        o->queue.size = o->queue.size - 1;
+        xsyncunlock(o->queue.sync);
+        xevent * event = __xevent_internal_process((xevent *) node->value.ptr, o);
+        if(event != node->value.ptr)
+        {
+            free(node->value.ptr);
+        }
+        if(event == xnil)
+        {
+            free(node);
+            node = xnil;
+        }
+        xsynclock(o->queue.sync);
+        if(event)
+        {
+            node->prev = o->queue.tail;
+            if(node->prev)
+            {
+                node->prev->next = node;
+            }
+            else
+            {
+                o->queue.head = node;
+            }
+            o->queue.tail = node;
+            o->queue.size = o->queue.size + 1;
+        }
+    }
+    xsyncunlock(o->queue.sync);
 }
 
 extern xint32 xeventenginerun(xeventengine * o)
@@ -15,9 +91,11 @@ extern xint32 xeventenginerun(xeventengine * o)
 
     if(o)
     {
-        xassertion(o->status != xeventengine_status_void, "engine is already running");
+        xassertion(xeventenginestatus(o) != xeventengine_status_void, "engine is already running");
 
         o->status |= xeventengine_status_on;
+
+        // TODO: PLUG EVENT GENERATOR
 
         while(o->status & xeventengine_status_cancel != xeventengine_status_cancel)
         {
@@ -25,64 +103,37 @@ extern xint32 xeventenginerun(xeventengine * o)
             {
                 xeventgenerator * generator = (xeventgenerator *) node->value.ptr;
             }
-            /**
-             * TODO: 스레드 풀을 만듭니다.
-             * TODO: 아래 로직을 함수로 만듭니다.
-             */
-            if(xtrue)
+            if(xlistsize(xaddressof(o->processors)) == 0 || o->status & xeventengine_status_main_cusuming_off)
             {
-                xsynclock(o->queue.sync);
-                xuint64 total = xlistsize(xaddressof(o->queue));
-                for(xuint64 i = 0; i < total && xlistsize(xaddressof(o->queue)) > 0; i++)
-                {
-                    xlistnode * node = o->queue.head;
-                    o->queue.head = node->next;
-                    if(o->queue.head)
-                    {
-                        o->queue.head->prev = xnil;
-                    }
-                    else
-                    {
-                        o->queue.tail = xnil;
-                    }
-                    o->queue.size = o->queue.size - 1;
-                    xsyncunlock(o->queue.sync);
-                    xevent * event = __xevent_internal_process((xevent *) node->value.ptr, o);
-                    if(event != node->value.ptr)
-                    {
-                        free(node->value.ptr);
-                    }
-                    if(event == xnil)
-                    {
-                        free(node);
-                        node = xnil;
-                    }
-                    xsynclock(o->queue.sync);
-                    if(event)
-                    {
-                        node->prev = o->queue.tail;
-                        if(node->prev)
-                        {
-                            node->prev->next = node;
-                        }
-                        else
-                        {
-                            o->queue.head = node;
-                        }
-                        o->queue.tail = node;
-                        o->queue.size = o->queue.size + 1;
-                    }
-                }
-                xsyncunlock(o->queue.sync);
+                __xeventengine_main_consume(o);
             }
-
             if(o->internal)
             {
                 o->internal(o);
             }
         }
-
+        // TODO: 모든 이벤트를 종료합니다.
         o->status &= (~xeventengine_status_on);
+
+        if(xeventengineallocated(o))
+        {
+            free(o);
+        }
     }
     return xsuccess;
+}
+
+extern xeventengine * xeventenginenew(void)
+{
+    xeventengine * o = (xeventengine *) malloc(sizeof(xeventengine));
+    xassertion(o == xnil, "fail to malloc (%d)", errno);
+
+    o->generators = xlistinit();
+    o->handler = xnil;
+    o->internal = xnil;
+    o->processors = xlistinit();
+    o->queue = xlistinit();
+    o->status = xeventengine_allocated;
+
+    return o;
 }
